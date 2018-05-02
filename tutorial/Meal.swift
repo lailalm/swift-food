4a24038cd36727f3d1ef41a1b1c50d3e23624bac
Mauhibah Laila <laila.mauhibah@gmail.com>
2018-05-02 19:46:51 +0700

    add data model

---
+++ tutorial/Meal.swift
@@ -0,0 +1,34 @@
+//
+//  Meal.swift
+//  tutorial
+//
+//  Created by Mauhibah Laila on 02/05/18.
+//  Copyright © 2018 Laila Mauhibah. All rights reserved.
+//
+
+import UIKit
+
+class Meal {
+    
+    // MARK: Properties
+    
+    var name: String
+    var photo: UIImage?
+    var rating: Int
+    
+    // MARK: Initialization
+    
+    init?(name: String, photo: UIImage?, rating: Int) {
+        
+        // Initialization should fail if there is no name or if the rating is negative.
+        if name.isEmpty || rating < 0  {
+            return nil
+        }
+        
+        // Initialize stored properties.
+        self.name = name
+        self.photo = photo
+        self.rating = rating
+        
+    }
+}
